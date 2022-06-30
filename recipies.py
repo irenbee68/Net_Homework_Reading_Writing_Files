de2ca8278fbd1c100d9ddfd5905f38506d7cb86b
irenbee68 <106485351+irenbee68@users.noreply.github.com>
2022-06-30 16:02:18 +0300

    Доработаны задачи по чтению файлов

--- recipies.py
+++ recipies.py
@@ -1,34 +1,34 @@
 from pprint import pprint
 
 
-file_name = "recipies.txt"
-def cook_book_dict(file_name):
-    with open(file_name, encoding="utf-8") as file_obj:
-        cook_book = {}
-        for line in file_obj:
-            dish_name = line.strip()
-            # print(dish_name) 
-            ingredients_all = []
-            name_key = ['ingredient_name', 'quantity', 'measure']
-            for item in range(int(file_obj.readline())):
-                ingredient = file_obj.readline()
-                ingredients = ingredient.strip().split('|')
-                dict_product = {}
-                dict_product[name_key[0]] = ingredients[0]
-                dict_product[name_key[1]] = ingredients[1]
-                dict_product[name_key[2]] = ingredients[2].strip()
-                ingredients_all.append(dict_product)
-            cook_book[dish_name] = ingredients
+# file_name = "recipies.txt"
+# def cook_book_dict(file_name):
+#     with open(file_name, encoding="utf-8") as file_obj:
+#         cook_book = {}
+#         for line in file_obj:
+#             dish_name = line.strip()
+#             # print(dish_name) 
+#             ingredients_all = []
+#             name_key = ['ingredient_name', 'quantity', 'measure']
+#             for item in range(int(file_obj.readline())):
+#                 ingredient = file_obj.readline()
+#                 ingredients = ingredient.strip().split('|')
+#                 dict_product = {}
+#                 dict_product[name_key[0]] = ingredients[0]
+#                 dict_product[name_key[1]] = ingredients[1]
+#                 dict_product[name_key[2]] = ingredients[2].strip()
+#                 ingredients_all.append(dict_product)
+#             cook_book[dish_name] = ingredients
         
             
-            cook_book[dish_name] = ingredients_all
-            # pprint(cook_book)
-            file_obj.readline()
+#             cook_book[dish_name] = ingredients_all
+#             # pprint(cook_book)
+#             file_obj.readline()
 
           
      
-    return cook_book
-pprint(cook_book_dict(file_name))
+#     return cook_book
+# pprint(cook_book_dict(file_name))
 
 cook_book = {'Запеченный картофель': [{'ingredient_name': 'Картофель ',
                            'measure': 'кг',
@@ -73,15 +73,16 @@ cook_book = {'Запеченный картофель': [{'ingredient_name': 'К
 
 def get_shop_list_by_dishes(dishes, person_count):
 
-    
+    cook_dict = {}
     for dish in dishes:
         
-        
         if dish in cook_book.keys():
-            cook_dict = {}
+            
             for ingredients in cook_book[dish]:
-
-                ingredients['quantity'] = ingredients['quantity'] * person_count
+                 
+                
+                ingredients['quantity'] = int(ingredients['quantity']) * person_count
+                
                 cook_dict[ingredients['ingredient_name']] = {'quantity': ingredients['quantity'],
                                                               'measure': ingredients['measure']}
 
@@ -91,4 +92,4 @@ def get_shop_list_by_dishes(dishes, person_count):
 
 
 
-pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Фахитос', 'Устрицы', 'Шампанское', 'Омлет', 'Омлет'], 2))
--- result.py
+++ result.py
@@ -1,46 +1,30 @@
-with open('1.txt', encoding="utf-8") as f:
-    lines = f.readlines()
-    print(type(lines))
-    print(len(lines))
-    for line in lines:
-        print(line, end="")
+import os
+from pprint import pprint
 
-with open('2.txt', encoding="utf-8") as f:
-    lines = f.readlines()
-    print(type(lines))
-    print(len(lines))
-    print(lines)
+# file for file in os.listdir('C:\Net_Homework\Net_Homework_Reading_Files\Net_Homework_Reading_Writing_Files\file_writing') 
+f_list_txt = [file for file in os.listdir('./file_writing') if file.endswith('.txt')]
+print(f_list_txt)
 
-with open('3.txt', encoding="utf-8") as f:
-    lines = f.readlines()
-    print(type(lines))
-    print(len(lines))
-    print(lines)
+dict_reader = {}
+
+for item in f_list_txt:
+    with open('./file_writing/' + item, encoding="utf-8") as f:
+        lines = f.readlines()
+        dict_value = [len(lines), lines]
+        dict_reader[item] = dict_value
+
+# pprint(dict_reader)        
+
+sorted_dict_reader = dict(sorted(dict_reader.items(), key=lambda item: item[1][0]))
+pprint(sorted_dict_reader)
 
 
-with open('result.txt', 'w') as f:
-    f.write('2.txt\n')
-    with open('2.txt') as content:
-        lines = content.readlines()
-        f.write(str(len(lines)))
-        f.write('\n')
-        for line in lines:
-            f.write(line)
-        f.write('\n')
-
-    f.write('1.txt\n')
-    with open('1.txt') as content:
-        lines = content.readlines()
-        f.write(str(len(lines)))
-        f.write('\n')
-        for line in lines:
-            f.write(line)
-        f.write('\n')
-            
-    f.write('3.txt\n')
-    with open('3.txt') as content:
-        lines = content.readlines()
-        f.write(str(len(lines)))
-        f.write('\n')
-        for line in lines:
-            f.write(line)
+with open('result.txt', 'w', encoding ="utf-8") as f:
+    for key, values in sorted_dict_reader.items():
+      f.write(key)
+      f.write('\n')
+      f.write(str(values[0]))
+      f.write('\n')
+      for line in values[1]:
+          f.write(line)
+      f.write('\n')
